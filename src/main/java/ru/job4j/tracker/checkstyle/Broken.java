bb58e99fecc4b1d13fc9692c8f0b2facdc06ccf8
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-06-07 23:44:47 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker.checkstyle;
+
+public class Broken {
+    private int sizeEmpty = 10;
+    String name;
+    public String surname;
+
+    public static final String NEW_VALUE = "";
+
+    void echo() { }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f) { }
+
+    Broken() { }
+
+}
