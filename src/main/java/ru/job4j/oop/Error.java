c81ade899a83302a24738cfd08a6dad45c284c97
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-06-10 22:27:38 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void getStatus() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error errorOne = new Error();
+        errorOne.getStatus();
+        Error errorTwo = new Error(true, 1, "On");
+        errorTwo.getStatus();
+        Error errorThree = new Error(false, 2, "Off");
+        errorThree.getStatus();
+    }
+}
