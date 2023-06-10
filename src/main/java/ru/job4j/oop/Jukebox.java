ca384bd8eee2c77c00110e412a12a68c325a50fa
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-06-10 13:55:51 +0300

    1.3. Вызов метода с аргументами. [#166086 #421962]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -1,20 +1,13 @@
 package ru.job4j.oop;
 
-import java.util.zip.Deflater;
-
 public class Jukebox {
     public void music(int position) {
-        switch (position) {
-            case (1):
-                System.out.println("Пусть бегут неуклюже");
-                break;
-            case (2):
-                System.out.println("Спокойной ночи");
-                break;
-            default:
-                System.out.println("Песня не найдена");
-                break;
-        }
+        String text = switch (position) {
+            case 1 -> "Пусть бегут неуклюже";
+            case 2 -> "Спокойной ночи";
+            default -> "Песня не найдена";
+        };
+        System.out.println(text);
     }
 
     public static void main(String[] args) {
