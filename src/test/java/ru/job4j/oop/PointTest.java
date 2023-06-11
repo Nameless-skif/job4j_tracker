cd1ef9e3d406870ef588cd098a44d8858fc3dd78
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-06-12 01:22:12 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -39,4 +39,22 @@ public class PointTest {
         double dist = a.distance(b);
         Assert.assertEquals(expected, dist, 0.01);
     }
+
+    @Test
+    public void when114to212then2dot23() {
+        double expected = 2.23;
+        Point a = new Point(1, 1, 4);
+        Point b = new Point(2, 1, 2);
+        double dist = a.distance3d(b);
+        Assert.assertEquals(expected, dist, 0.01);
+    }
+
+    @Test
+    public void when114to212then3dot16() {
+        double expected = 3.16;
+        Point a = new Point(1, 1, 6);
+        Point b = new Point(2, 1, 9);
+        double dist = a.distance3d(b);
+        Assert.assertEquals(expected, dist, 0.01);
+    }
 }
