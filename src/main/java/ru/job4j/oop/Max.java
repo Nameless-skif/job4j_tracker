1d91716b2bf7bf470696ea3a917672cefc3beb41
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-06-12 10:48:03 +0300

    3. Перегрузить метод max для трех чисел [#122644 #422506]

--- src/main/java/ru/job4j/oop/Max.java
+++ src/main/java/ru/job4j/oop/Max.java
@@ -2,15 +2,14 @@ package ru.job4j.oop;
 
 public class Max {
     public static int max(int a, int b) {
-        int result = a > b ? a : b;
-        return result;
+        return a > b ? a : b;
     }
 
     public static int max(int a, int b, int c) {
-         return (max(a, b) > c) ? max(a, b) : c;
+         return max(max(a, b), c);
     }
 
     public static int max(int a, int b, int c, int d) {
-        return (max(a, b, c) > d) ? max(a, b, c) : d;
+        return max(max(a, b, c), d);
     }
 }
