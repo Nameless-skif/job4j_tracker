e6fde86f59d83fb4d0776b600bdb12c23fcdf8e9
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-06-12 10:31:43 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642 #422504]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -16,4 +16,15 @@ class TriangleTest {
         double expected = 8;
         assertThat(rsl).isCloseTo(expected, offset(0.001));
     }
+
+    @Test
+    public void when00and140and50ThenNot() {
+        Point a = new Point(0, 0);
+        Point b = new Point(14, 0);
+        Point c = new Point(5, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = -1;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
 }
