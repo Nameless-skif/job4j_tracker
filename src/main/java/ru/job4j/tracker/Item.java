c8f7e9c19cef436adfe04f3ed5db5ddab7ce1bcd
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-06-25 21:41:08 +0300

    10. toString [#310018 #424879]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -9,15 +9,6 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    @Override
-    public String toString() {
-        return "Item{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER)
-                + '}';
-    }
-
     public Item() {
     }
 
@@ -28,6 +19,15 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", created=" + created.format(FORMATTER)
+                + '}';
     }
 
     public LocalDateTime getCreated() {
