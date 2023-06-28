84f2bc927ee8861412cbb416b6cd37ff676e1c56
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-06-28 21:46:02 +0300

    2.0. Чтение из консоли. Класс Scanner. [#179514 # [#179514 #426042]]

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -9,15 +9,11 @@ public class MagicBall {
         Scanner input = new Scanner(System.in);
         String question = input.nextLine();
         int answer = new Random().nextInt(3);
-        switch (answer) {
-            case 0:
-                System.out.println("Да");
-                break;
-            case 1:
-                System.out.println("Нет");
-                break;
-            default:
-                System.out.println("Может быть");
-        }
+        String rsl = switch (answer) {
+            case 0 -> "Да";
+            case 1 -> "Нет";
+            default -> "Может быть";
+        };
+        System.out.println(rsl);
     }
 }
