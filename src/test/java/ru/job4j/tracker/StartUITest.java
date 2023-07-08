7404606228aa0180d400271480d8f7b57ac7ae6c
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-07-08 20:38:35 +0300

    9. Тестирование. Подготовка данных. [#182960  [#182960]]

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,36 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StartUITest {
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    public void whenDeleteItem() {
+            Tracker tracker = new Tracker();
+            Item item = new Item("new item");
+            tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "new item"
+        };
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted).isEqualTo(null);
+        }
+    }
+
