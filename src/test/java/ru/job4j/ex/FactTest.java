55aa37eb40aeec179cc7fcbae5457163dc7ded5b
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-08-01 21:06:25 +0300

    0.3. Тестирование исключений с junit [#219366  [#219366]]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FactTest {
+    @Test
+    public void whenFact3Res6() {
+        int start = 0;
+        int finish = 3;
+        int expected = 6;
+        int result = Fact.calc(3);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Fact.calc(-1);
+                });
+        assertThat(exception.getMessage()).isEqualTo("N could not be less than 0");
+    }
+}
