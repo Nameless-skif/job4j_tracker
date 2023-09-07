836b7f96c16f4f4dc5261228149a17b02a3eea84
Nameless-skif <123570283+Nameless-skif@users.noreply.github.com>
2023-09-07 23:15:17 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,17 +1,36 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+    private final Output out;
+    private final Input in;
+
+    public ValidateInput(Output out, Input input) {
+        this.out = out;
+        this.in = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return in.askStr(question);
+    }
 
     @Override
     public int askInt(String question) {
         boolean invalid = true;
         int value = -1;
+        char[] check = question.toCharArray();
         do {
             try {
-                value = super.askInt(question);
+                value = in.askInt(question);
                 invalid = false;
+                for (char num : check) {
+                    if (num > 48 || num < 57) {
+                        break;
+                    }
+                }
+
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter valid data");
+                out.println("Please enter valid data");
             }
         } while (invalid);
         return value;
